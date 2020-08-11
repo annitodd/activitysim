979383bc77bc81fc4430f99f98641b1e89d957d9
mxndrwgrdnr <magardner@berkeley.edu>
2020-08-11 23:14:18 +0000

    write h5 outputs to usim input dir

--- activitysim/abm/models/write_outputs_to_s3.py
+++ activitysim/abm/models/write_outputs_to_s3.py
@@ -20,18 +20,27 @@ def write_outputs_to_s3(data_dir, settings):
 
     logger.info("Writing outputs to s3!")
 
-    # LOAD ASIM OUTPUTS
+    # 1. LOAD ASIM OUTPUTS
     output_tables_settings = settings['output_tables']
+    h5_output = output_tables_settings['h5_output']
     prefix = output_tables_settings['prefix']
     output_tables = output_tables_settings['tables']
 
     asim_output_dict = {}
-    for table_name in output_tables:
-        file_name = "%s%s.csv" % (prefix, table_name)
-        file_path = config.output_file_path(file_name)
-        asim_output_dict[table_name] = pd.read_csv(file_path)
 
-    # LOAD USIM INPUTS
+    if h5_output is False:
+        for table_name in output_tables:
+            file_name = "%s%s.csv" % (prefix, table_name)
+            file_path = config.output_file_path(file_name)
+            asim_output_dict[table_name] = pd.read_csv(file_path)
+    else:
+        file_name = '%soutput_tables.h5' % prefix
+        file_path = config.output_file_path(file_name)
+        store = pd.HDFStore(file_path)
+        for table_name in output_tables:
+            asim_output_dict[table_name] = store[table_name]
+
+    # 2. LOAD USIM INPUTS
     data_store_path = os.path.join(data_dir, settings['usim_data_store'])
 
     if not os.path.exists(data_store_path):
@@ -46,7 +55,7 @@ def write_outputs_to_s3(data_dir, settings):
     households_cols = store['households'].reset_index().columns
     persons_cols = store['persons'].reset_index().columns
 
-    # UPDATE USIM PERSONS
+    # 3. UPDATE USIM PERSONS
     # new columns to persist: workplace_taz, school_taz
     p_names_dict = {'PNUM': 'member_id'}
     asim_p_cols_to_include = ['workplace_taz', 'school_taz']
@@ -58,7 +67,7 @@ def write_outputs_to_s3(data_dir, settings):
         asim_output_dict['persons'] = asim_output_dict['persons'][
             list(persons_cols) + asim_p_cols_to_include]
 
-    # UPDATE USIM HOUSEHOLDS
+    # 4. UPDATE USIM HOUSEHOLDS
     # no new columns to persist, just convert auto_ownership --> cars
     hh_names_dict = {
         'HHID': 'household_id',
@@ -77,7 +86,7 @@ def write_outputs_to_s3(data_dir, settings):
         asim_output_dict['households'] = asim_output_dict[
             'households'][households_cols]
 
-    # WRITE OUT
+    # 5. WRITE OUT FOR BEAM
     archive_name = 'asim_outputs.zip'
     outpath = config.output_file_path(archive_name)
     logger.info(
@@ -122,9 +131,34 @@ def write_outputs_to_s3(data_dir, settings):
             fs.mv(remote_s3_path, new_fpath)
     logger.info('Sending combined data to s3!')
     fs.put(outpath, remote_s3_path)
-
-    store.close()
-
     logger.info(
         'Zipped archive of results for use in UrbanSim or BEAM now available '
         'at {0}'.format("s3://" + remote_s3_path))
+
+    # 6. WRITE OUT FOR USIM
+    usim_archive_name = 'model_data.h5'
+    outpath_usim = config.output_file_path(usim_archive_name)
+    usim_bucket = bucket.replace('activitysim', 'urbansim')
+    usim_remote_s3_path = os.path.join(
+        usim_bucket, 'input', scenario, year, usim_archive_name)
+    logger.info(
+        'Merging results back into UrbanSim format and storing as .h5!')
+    out_store = pd.HDFStore(outpath_usim)
+    # copy usim static inputs into archive
+    for table_name in store.keys():
+        if table_name not in [
+                '/persons', '/households', 'persons', 'households']:
+            out_store.put(table_name, store[table_name])
+
+    # copy usim outputs into archive
+    for table_name in asim_output_dict.keys():
+        out_store.put(table_name, asim_output_dict[table_name])
+
+    out_store.close()
+    logger.info("Copying outputs to UrbanSim inputs!")
+    fs.put(outpath_usim, usim_remote_s3_path)
+    logger.info(
+        'New UrbanSim model data now available '
+        'at {0}'.format("s3://" + usim_remote_s3_path))
+
+    store.close()
